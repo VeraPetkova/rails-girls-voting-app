3d791ba3ca1c4a78b553a807580a61a5416b54c6
Vera <mar0sis@gmail.com>
2022-05-14 11:35:01 +0300

    initial commit

---
+++ myapp.rb
@@ -0,0 +1,33 @@
+# myapp.rb
+require 'sinatra'
+require 'yaml/store'
+
+get '/' do
+  @title = 'Добре дошли в машината за гласуване!'
+  erb :index
+end
+
+get '/cast' do
+  @title = 'Благодарим за вашия глас!'
+  @vote  = params['vote']
+
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    if @store['votes'] == nil
+      @store['votes'] = {}
+    end
+
+    @store['votes'][@vote] = @store['votes'].fetch(@vote, 0) + 1
+  end
+
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+
+  erb :results
+end
